8bde2cac3961e783050abd8b61cf46d543a44058
xlemilyhaha-create <xlemilyhaha@gmail.com>
2025-12-02 13:20:55 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,17 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+  // Load env file based on `mode` in the current working directory.
+  // Setting the third parameter to '' allows loading variables without VITE_ prefix
+  const env = loadEnv(mode, '.', '');
+
+  return {
+    plugins: [react()],
+    define: {
+      // Vital: This replaces 'process.env.API_KEY' with the actual string value during the build
+      'process.env.API_KEY': JSON.stringify(env.API_KEY),
+    },
+  };
 });
